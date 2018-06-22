a898f11d783f62fb099401400255d796013a7a27
kekaadrenalin <kekapor@outlook.com>
2018-06-22 18:14:31 +0300

    small update in ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -5,6 +5,8 @@ chmod 700 ~/.ssh
 nano ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys
 
+read -r -p "Copy your public key to the clipboard and paste it into the text box. To continue, press any key..." response
+
 echo "PubkeyAuthentication yes
 PasswordAuthentication no
 
@@ -18,3 +20,5 @@ Subsystem       sftp    /usr/lib/openssh/sftp-server
 clear
 
 sudo systemctl reload sshd
+
+echo "SSH configure is success.\n"
