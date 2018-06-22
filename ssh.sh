8b2adff2b85027eaffe59dae95fa3ee17b341e45
kekaadrenalin <kekapor@outlook.com>
2018-06-22 18:16:09 +0300

    small update in ssh.sh #2

--- ssh.sh
+++ ssh.sh
@@ -2,10 +2,11 @@
 
 mkdir ~/.ssh
 chmod 700 ~/.ssh
-nano ~/.ssh/authorized_keys
-chmod 600 ~/.ssh/authorized_keys
 
 read -r -p "Copy your public key to the clipboard and paste it into the text box. To continue, press any key..." response
+nano ~/.ssh/authorized_keys
+
+chmod 600 ~/.ssh/authorized_keys
 
 echo "PubkeyAuthentication yes
 PasswordAuthentication no
