ed7d15ae0be7173e0df5dd009bb15c4be3f96460
kekaadrenalin <kekapor@outlook.com>
2018-08-28 22:26:56 +0300

    add after-os-install main file

---
+++ after-install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~
+
+# get ssh
+wget -O ssh.sh https://raw.githubusercontent.com/kekaadrenalin/after-os-install/master/ssh.sh
+
+# get webmin
+wget -O webmin-instal.sh https://raw.githubusercontent.com/kekaadrenalin/after-os-install/master/webmin-install.sh
+
+#get server
+wget -O server-install.sh https://raw.githubusercontent.com/kekaadrenalin/after-os-install/master/server-install.sh
